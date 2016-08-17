f3abe0daf9530e778fca81e8d72d6ea406d4a68e
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-08-17 10:43:38 -0400

    wip; implementing paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816130348) do
+ActiveRecord::Schema.define(version: 20160817143133) do
 
   create_table "destinations", force: :cascade do |t|
     t.string   "city"
     t.string   "country"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.float    "population"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
